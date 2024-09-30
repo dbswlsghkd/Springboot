9c532eb978439e766bf1d2ce3e1154d5172f11f0
황윤진 <dbswlsghkd@gmail.com>
2024-09-30 22:31:47 +0900

    mssql db 연동, product 테이블 자동 생성 완료

---
+++ src/main/java/com/example/testproject/data/dao/impl/ProductDAOImpl.java
@@ -0,0 +1,32 @@
+package com.example.testproject.data.dao.impl;
+
+import com.example.testproject.data.dao.ProductDAO;
+import com.example.testproject.data.entity.ProductEntity;
+import com.example.testproject.data.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductDAOImpl implements ProductDAO {
+
+    ProductRepository productRepository;
+
+    // 자동 연결
+    @Autowired
+    public ProductDAOImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public ProductEntity saveProduct(ProductEntity productEntity) {
+
+        productRepository.save(productEntity);
+        return productEntity;
+    }
+
+    @Override
+    public ProductEntity getProduct(String productId){
+        ProductEntity productEntity = productRepository.getById(productId);
+        return productEntity;
+    }
+}
