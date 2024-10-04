f45e0d341670603a2abac561ad6ae59b0e5580a1
황윤진 <dbswlsghkd@gmail.com>
2024-10-04 22:16:45 +0900

    예외처리 @ContrllerAdvice, @ExceptionHandler 사용

--- src/main/java/com/example/testproject/controller/HelloController.java
+++ src/main/java/com/example/testproject/controller/HelloController.java
@@ -3,7 +3,13 @@ package com.example.testproject.controller;
 import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 // 컨트롤러
@@ -27,4 +33,26 @@ public class HelloController {
         LOGGER.error("Error Log");
 
     }
+
+    @PostMapping("/exception")
+    public void exceptionTest() throws Exception{
+        throw new Exception("Test Exception");
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<Map<String, String>> ExceptionHandler(Exception ex){
+        HttpHeaders responseHeaders = new HttpHeaders();
+        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+
+        LOGGER.error(ex.getLocalizedMessage());
+        LOGGER.info("Controller 내 ExceptionHandler 호출");
+
+        Map<String, String> map = new HashMap<>();
+        map.put("error type", httpStatus.getReasonPhrase());
+        map.put("code", "400");
+        map.put("message", "에러 발생");
+
+        return new ResponseEntity<>(map, responseHeaders, httpStatus);
+    }
+
 }
